6cd7849297530b3ae7870e10f3bc700ee759c966
Mentat_Radnor <kholosinc@gmail.com>
2021-01-24 09:44:02 +0300

    Lesson2 update

--- index.php
+++ index.php
@@ -1,34 +1,158 @@
 <?php
 
+
+// Задание 1
+$min = -100;
+$max = 100;
+
+
 // define('WORD', 'Kavabanga');
-$newDate = date('l jS \of F Y h:i:s A');
-// echo WORD;
+$a = rand($min, $max);
+$b = rand($min, $max);
+
+echo $a . "<br/>";
 
 
-
-$a = 4;
-$b = 5;
-var_dump($a == $b);
+echo $b . "<br/>";
 
 
+if ($a > 0 && $b > 0)
+    echo  $a + $b;
+else if ($a < 0 && $b < 0)
+    echo $a * $b;
+else
+    echo $a + $b;
+
+echo "<br/> Задача 2";
+// задание 2
+$a = rand(0, 15);
+
+switch ($a) {
+    case 0:
+        echo "<br/> Число 0";
+        break;
+    case 1:
+        echo "<br/> Число 1";
+        break;
+    case 2:
+        echo "<br/> Число 2";
+        break;
+    case 3:
+        echo "<br/> Число 3";
+        break;
+    case 4:
+        echo "<br/> Число 4";
+        break;
+    case 5:
+        echo "<br/> Число 5";
+        break;
+    case 6:
+        echo "<br/> Число 6";
+        break;
+    case 7:
+        echo "<br/> Число 7";
+        break;
+    case 8:
+        echo "<br/> Число 8";
+        break;
+    case 9:
+        echo "<br/> Число 9";
+        break;
+    case 10:
+        echo "<br/> Число 10";
+        break;
+    case 11:
+        echo "<br/> Число 11";
+        break;
+    case 12:
+        echo "<br/> Число 12";
+        break;
+    case 13:
+        echo "<br/> Число 13";
+        break;
+    case 14:
+        echo "<br/> Число 14";
+        break;
+    case 15:
+        echo "<br/> Число 15 <br/>";
+        break;
+}
+
+echo "<br/> Задача 3 <br/>";
+
+function sum($a, $b)
+{
+    $a += $b;
+    echo $a . "<br/>";
+}
+
+sum($a, $b);
+
+function subt($a, $b)
+{
+    $a -= $b;
+    echo $a . "<br/>";
+}
+
+subt($a, $b);
+
+function multipl($a, $b)
+{
+    $a *= $b;
+    echo $a . "<br/>";
+}
+
+multipl($a, $b);
+
+function div($a, $b)
+{
+    $a /= $b;
+    echo $a . "<br/>";
+}
+
+Div($a, $b);
+
+echo "<br/> Задача 4 <br/>";
+
+function mathOperation($arg1, $arg2, $operation)
+{
+    switch ($operation) {
+        case "Умножение":
+            multipl($arg1, $arg2);
+            break;
+        case "Деление":
+            div($arg1, $arg2);
+            break;
+        case "Вычитание":
+            subt($arg1, $arg2);
+            break;
+        case "Сложение":
+            sum($arg1, $arg2);
+            break;
+    }
+}
+
+mathOperation(5, 10, "Умножение");
+
+echo "<br/>";
+
+$today = date("Y");
+
+echo $today;
 
 
-$a = 5;
-$b = '05';
-var_dump($a == $b);                             // отбрасывает 0 и сравнивает 2 значения. Так как это не строгое сравнение, то выдает true
-var_dump((int)'012345') . "\n";                  // Так как мы не указали количество чисел, то 0 отбрасывается 
-var_dump((float)123.0 === (int)123.0) . "\n"; // разные типы?
-var_dump((int)0 === (int)'hello, world') . "\n"; // Скорее всего, происходит проверка на наличие чисел, если их нет то слово приводится к 0. Если бы мы поставили в начале 1, то было false
-var_dump((int)0 === (int)'1hello, world') . "\n";
+echo "<br/> Задача 5 <br/>";
 
+function power($val, $pow)
+{
+    if (
+        $val == 1
+    ) return $pow;
+    else return $pow * power(--$val, $pow);
+}
 
-$a = 1;
-$b = 2;
-$a = $a + $b;
-$b = $a - $b;
-$a = $a - $b;
+echo power(4, 4);
 
-echo $a . $b;
 ?>
 
 <!DOCTYPE html>
